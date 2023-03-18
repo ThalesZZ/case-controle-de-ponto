fab55b1210b9f0bd58f2d6204893f7cb47e118b0
ThalesZZ <thazars@hotmail.com>
2023-03-17 22:50:56 -0300

    init node server with express

---
+++ index.ts
@@ -0,0 +1,21 @@
+
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World From the Typescript Server!')
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
