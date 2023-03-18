e1a636844f7e0d2bb9f703a6fe5c321085b051b8
ThalesZZ <thazars@hotmail.com>
2023-03-17 22:59:48 -0300

    init react server side

---
+++ client/src/components/form-input/form-input.tsx
@@ -0,0 +1,20 @@
+import { FC, InputHTMLAttributes } from "react";
+
+type FromInputProps = { label: string } &
+  InputHTMLAttributes<HTMLInputElement>;
+
+const FormInput: FC<FromInputProps> = ({ label, ...otherProps }) => {
+  return (
+    <div className="group">
+      <input {...otherProps} />
+      {
+        label &&
+        <div className="form-input-label">
+          {label}
+        </div>
+      }
+    </div>
+  );
+}
+
+export default FormInput;
