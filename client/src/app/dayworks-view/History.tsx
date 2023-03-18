ed7acb6b6eb97fecce47257e0a1eda20b64ff175
ThalesZZ <thazars@hotmail.com>
2023-03-18 16:03:00 -0300

    styled dayworks entries

--- client/src/app/dayworks-view/History.tsx
+++ client/src/app/dayworks-view/History.tsx
@@ -1,23 +1,31 @@
+import moment from 'moment'
 import React from "react"
 import styled, { css } from 'styled-components'
 import { DayWork } from "../../../../src/models/DayWork"
+
+const padTimeValue = (value: number): string => value.toString().padStart(2, '0')
 
 interface HistoryProps {
     dayworks: Array<DayWork>
 }
 
 export function History({ dayworks }: HistoryProps): React.ReactElement {
+    const entryType: 'checkin' | 'checkout' = 'checkin'
+
     return (
         <Container>
             <label>Dias anteriores</label>
             <div id="entries">
                 {dayworks.map(daywork => {
-                    
+                    const dateTime = moment(daywork[entryType])
+                    const date = dateTime.format('DD/MM/YY')
+                    const hours = padTimeValue(dateTime.hour())
+                    const minutes = padTimeValue(dateTime.minute())
 
                     return (
                         <div key={daywork.id} className="entry">
-                            <span>{daywork.checkin.toString()}</span>
-                            <span>{daywork.checkin.toString()}</span>
+                            <span>{date}</span>
+                            <span>{`${hours}h ${minutes}m`}</span>
                         </div>
                     )
                 })}
@@ -30,14 +38,28 @@ const Container = styled.div`
     ${({ theme }) => css`
         flex-flow: column;
         gap: 0.5em;
+        overflow-y: auto;
 
         #entries {
             flex-flow: column;
+            overflow-y: auto;
             gap: 0.5em;
 
             .entry {
-                padding: 0.9em;
+                justify-content: space-between;
+                padding: 1em;
+                padding-right: 1.5em;
                 background-color: ${theme.auxiliar};
+
+                span:first-child {
+                    color: ${theme.auxiliarTextColor};
+                    font-weight: 500;
+                }
+
+                span:last-child {
+                    color: ${theme.textColor};
+                    font-weight: 700;
+                }
             }
         }
     `}
