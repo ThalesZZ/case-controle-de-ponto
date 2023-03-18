b011e1bd04bec10f49e0dc8d37dfa325b579844d
ThalesZZ <thazars@hotmail.com>
2023-03-17 23:27:15 -0300

    clean react app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,6 @@
-
-import React from "react";
-
-import './App.css';
-
-// TypeScript declarations
-type User = {
-  id: number,
-  name: string,
-  email: string,
-  password: string
-}
-
 const App = () => {
-  const [users, setUsers] = React.useState<User[]>([])
-
-  React.useEffect(() => {
-    fetch('http://localhost:8000/users', {
-      method: 'GET',
-      headers: { 'Content-type': 'application/json' }
-    })
-      .then(response => response.json())
-      .then(response => {
-        console.log(response)
-      })
-  }, [])
-
   return (
-    <ul>
-      {users.map(user => (
-        <li key={user.id}> {user.name} </li>
-      ))}
-    </ul>
+    <div>test</div>
   );
 }
 
